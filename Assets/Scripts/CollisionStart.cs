e7abbe4d3ef3d7fc132eac6ae3f4bbee5498556c
Tyler Dalke <TylerADalke22@hotmail.com>
2023-11-05 17:37:11 -0600

    Added Unity 2019.4.16f project with basic objects and scripts.

---
+++ Assets/Scripts/CollisionStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionStart : MonoBehaviour
+{
+    Rigidbody[] rigidbodies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Bat") {
+            
+            foreach (Rigidbody rb in rigidbodies) {
+                rb.isKinematic = false;
+            }
+        }
+    }
+}
